8e8027270f3f1b8d161ed02ea4bc4745ed6959da
Ruan Lobaque <165332999+lobaque29@users.noreply.github.com>
2025-10-31 14:32:38 -0300

    Add files via upload
    Exercicios capitulo 9 IFMG-DESENVOLVIMENTO FRONT END

---
+++ indexaula9.js
@@ -0,0 +1,8 @@
+const carros = ["volvo", "bmw", "mercedes","fiat","chevrolet", "volkswagen","ford","Honda"];
+document.getElementById("demo").innerHTML = carros
+
+function ListaCarros(){
+  console.log(carros);
+}
+
+
